87b0e03f21e96d895f22872ebca910edaec315b9
Robert Medina <csoperativo@gmail.com>
2024-04-07 03:09:06 -1000

    commit fix

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,4 +1,4 @@
-export {Navbar} from "./Navbar/Navbar"
+export { Navbar } from "./Navbar/Navbar"
 export { Footer } from "./Footer/Footer"
 export { Description } from "./home/Description/Description"
 export { Hero } from "./home/Hero/Hero"
