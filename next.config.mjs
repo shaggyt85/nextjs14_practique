abc9dcc9688fbfa4f4933c114a386718f75d0e79
Robert Medina <csoperativo@gmail.com>
2024-04-07 03:06:44 -1000

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
         includePaths: [path.join(__dirname, "src/sass")],
         prependData: `@import "main.sass"`,
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn.shopify.com",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
--- src/app/components/home/MainProduction/MainProduction.tsx
+++ src/app/components/home/MainProduction/MainProduction.tsx
@@ -1,7 +1,36 @@
-export const MainProduction = () => {
+import Image from "next/image"
+
+const getProducts = async () => {
+    try {
+        const response =  await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+            headers: new Headers({
+                'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ''
+            })
+        })
+        const {products} = await response.json()
+        return products
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const MainProduction = async () => {
+    const products = await getProducts()
+    console.log(products)
     return (
         <section>
-            <p>secciones</p>
+            <h3>New Products Released</h3>
+            <div>
+                {products?.map((product: any) => {
+                    const imageSrc = product.images[0]?.src;
+                    return (
+                        <article key={products.id}>
+                            <p>{products.title}</p>
+                            <Image src={imageSrc} fill alt={product.title} loading="eager" />
+                        </article>
+                    );
+                })}
+            </div>
         </section>
     )
 }
